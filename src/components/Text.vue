717ce5296d681d16206d1980c5f434fa87c69a10
chenjianpeng <1210929612@qq.com>
2018-01-24 14:11:06 +0800

    text和input添加了一些修改

--- src/components/Text.vue
+++ src/components/Text.vue
@@ -1,7 +1,9 @@
 <template>
   <ui-element :comData="comData">
-    {{comData.text}}
-  </ui-element>  
+    <div :style="eleStyle" class="text-wrap">
+      {{comData.text}}
+    </div>
+  </ui-element>
 </template>
 
 <script>
@@ -12,9 +14,30 @@ export default {
     },
   },
   methods: {
-    
+
   },
-  data(){
+  computed: {
+    eleStyle: function() {
+
+      let style = {
+        'width': '100%',
+        'height': '100%'
+      }
+
+      for (let key in this.comData.s) {
+        let val = this.comData.s[key]
+
+        if (key === 'boxShadow') {
+          style['box-shadow'] = '' + (val.color || '#fff') + ' ' + (val.position || '0 0') + ' ' + (val.blur || '0') + ' ' + (val.radius || '0')
+          continue
+        }
+        style[key] = val
+      }
+
+      return style
+    }
+  },
+  data() {
     return {
 
     }
@@ -23,5 +46,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.text-wrap{
+  display: flex;
+}
 </style>
