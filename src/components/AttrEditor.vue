5d73393430cf26de5ffb616ea08073ecf47b75ce
chenjianpeng <1210929612@qq.com>
2018-01-25 15:48:21 +0800

    表单样式简单修改

--- src/components/AttrEditor.vue
+++ src/components/AttrEditor.vue
@@ -1,53 +1,89 @@
 <template>
   <div>
-    <div class="field">
-      <button class="button" @click="delel">删除该元素</button>
+    <div class="field is-horizontal">
+      <button class="button is-small" @click="delel">删除该元素</button>
     </div>
-    <div class="field">
-      <label class="label">文本</label>
-      <div class="control">
-        <input class="input" type="text" v-model="$store.state.editorData.text">
+    <div class="field is-horizontal">
+      <div class="field-label is-small">
+        <label class="label">文本</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <div class="control">
+            <input class="input is-small" type="text" v-model="$store.state.editorData.text">
+          </div>
+        </div>
       </div>
     </div>
-    <div class="field">
-      <label class="label">X坐标</label>
-      <div class="control">
-        <input class="input" type="number" v-model.number="$store.state.editorData.top">
+    <div class="field is-horizontal">
+      <div class="field-label is-small">
+        <label class="label">X坐标</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <div class="control">
+            <input class="input is-small" type="number" v-model.number="$store.state.editorData.top">
+          </div>
+        </div>
       </div>
     </div>
-    <div class="field">
-      <label class="label">Y坐标</label>
-      <div class="control">
-        <input class="input" type="number" v-model.number="$store.state.editorData.left">
+    <div class="field is-horizontal">
+      <div class="field-label is-small">
+        <label class="label">Y坐标</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <div class="control">
+            <input class="input is-small" type="number" v-model.number="$store.state.editorData.left">
+          </div>
+        </div>
       </div>
     </div>
-    <div class="field">
-      <label class="label">宽度</label>
-      <div class="control">
-        <input class="input" type="number" v-model.number="$store.state.editorData.width">
+    <div class="field is-horizontal">
+      <div class="field-label is-small">
+        <label class="label">宽度</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <div class="control">
+            <input class="input is-small" type="number" v-model.number="$store.state.editorData.width">
+          </div>
+        </div>
       </div>
     </div>
-    <div class="field">
-      <label class="label">高度</label>
-      <div class="control">
-        <input class="input" type="number" v-model.number="$store.state.editorData.height">
+    <div class="field is-horizontal">
+      <div class="field-label is-small">
+        <label class="label">高度</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <div class="control">
+            <input class="input is-small" type="number" v-model.number="$store.state.editorData.height">
+          </div>
+        </div>
       </div>
     </div>
-    <div class="field">
-      <label class="label">图层</label>
-      <div class="control" @click="operateLayer">
-        <a class="forward button">
-          ↑
-        </a>
-        <a class="top button">
-          top
-        </a>
-        <a class="backward button">
-          ↓
-        </a>
-        <a class="bottom button">
-          bottom
-        </a>
+    <div class="field is-horizontal">
+      <div class="field-label is-small">
+        <label class="label">图层</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <div class="control" @click="operateLayer">
+            <a class="forward button is-small">
+              ↑
+            </a>
+            <a class="top button is-small">
+              top
+            </a>
+            <a class="backward button is-small">
+              ↓
+            </a>
+            <a class="bottom button is-small">
+              bottom
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -64,7 +100,7 @@ export default {
     }
   },
   methods: {
-    operateLayer (event) {
+    operateLayer(event) {
       let btnType = event.target.getAttribute('class').split(' ')[0]
       this.$store.commit('resetLayer', btnType)
     },
@@ -76,5 +112,7 @@ export default {
 </script>
 
 <style lang='less' scoped>
-
+.label {
+  width: 50px;
+}
 </style>
--- src/components/edit/ButtonEditor.vue
+++ src/components/edit/ButtonEditor.vue
@@ -2,107 +2,209 @@
   <div>
     <attr-editor></attr-editor>
     <div class="subEditor">
-      <div class="field">
-        <label class="label">背景图片</label>
-        <div class="control">
-          <FileUpLoad @urlChange="fileChange"></FileUpLoad>
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">背景图片</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <FileUpLoad @urlChange="fileChange"></FileUpLoad>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">背景色</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s['background-color']">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">背景色</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="color" v-model="$store.state.editorData.s['background-color']">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">透明度</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s['opacity']">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">透明度</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="text" v-model="$store.state.editorData.s['opacity']">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">内边距</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s['padding']">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">内边距</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="text" v-model="$store.state.editorData.s['padding']">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">内部对齐方式</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s['justify-content']">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">对齐方式</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="text" v-model="$store.state.editorData.s['justify-content']">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">边框样式</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s['border-style']">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">边框样式</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="text" v-model="$store.state.editorData.s['border-style']">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">边框大小</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s['border-width']">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">边框大小</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="text" v-model="$store.state.editorData.s['border-width']">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">边框颜色</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s['border-color']">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">边框颜色</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="color" v-model="$store.state.editorData.s['border-color']">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">边框圆角大小</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s['border-radius']">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">圆角大小</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="text" v-model="$store.state.editorData.s['border-radius']">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">字体</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s['font-family']">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">字体</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="text" v-model="$store.state.editorData.s['font-family']">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">字号</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s['font-size']">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">字号</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="text" v-model="$store.state.editorData.s['font-size']">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">字体粗细</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s['font-weight']">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">字体粗细</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="text" v-model="$store.state.editorData.s['font-weight']">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">字体颜色</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s['color']">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">字体颜色</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="color" v-model="$store.state.editorData.s['color']">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">阴影颜色</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s.boxShadow.color">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">阴影颜色</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="color" v-model="$store.state.editorData.s.boxShadow.color">
+            </div>
+          </div>
         </div>
       </div>
 
-      <div class="field">
-        <label class="label">阴影大小</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s.boxShadow.radius">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">阴影大小</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="text" v-model="$store.state.editorData.s.boxShadow.radius">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">阴影方向</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s.boxShadow.position">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">阴影方向</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="text" v-model="$store.state.editorData.s.boxShadow.position">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="field">
-        <label class="label">阴影模糊</label>
-        <div class="control">
-          <input class="input" type="text" v-model="$store.state.editorData.s.boxShadow.blur">
+      <div class="field is-horizontal">
+        <div class="field-label is-small">
+          <label class="label">阴影模糊</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="text" v-model="$store.state.editorData.s.boxShadow.blur">
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -128,5 +230,9 @@ export default {
 <style lang="less" scoped>
 .subEditor {
   margin-top: 0.75rem;
+}
+
+.label {
+  width: 50px;
 }
 </style>
